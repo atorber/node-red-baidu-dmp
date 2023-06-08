a03bf6fd9d976ec3cf41a8407ff695ba336a9648
LuChao <atorber@163.com>
2023-06-08 14:05:01 +0800

    Update baidu-dmp.js

--- baidu-dmp.js
+++ baidu-dmp.js
@@ -13,30 +13,40 @@ module.exports = function(RED) {
         // 定义接口路径
         var path = `/v1/devices/${instanceId}/${productkey}/${deviceName}/resources`;
         
-        // 注册输入事件监听器
-        node.on('input', function(msg) {
-            // 获取输入消息中的resourceType参数，默认为 "MQTT"
-            var resourceType = msg.payload.resourceType || "MQTT";
-            
-            // 定义请求参数并将其转换为JSON字符串，去掉空格
-            var bodyString = JSON.stringify({ resourceType }, null, 0);
-            
-            // 计算分钟级时间戳
-            var timestamp = Math.floor(Date.now() / (1000 * 60));
-            
-            // 计算请求签名
-            var signature = createSignature(deviceSecret, path, bodyString, timestamp);
-            
-            // 构建请求头
-            var headers = {
-                'Content-Type': 'application/json',
-                'expiryTime': timestamp,
-                'signature': signature
-            };
-            
-            // 发送请求
-            sendRequest(node, host + path, headers, { resourceType }, msg);
-        });
+// 注册输入事件监听器
+node.on('input', function(msg) {
+    // 获取输入消息中的resourceType参数，默认为 "MQTT"
+    var resourceType = msg.payload.resourceType || "MQTT";
+    
+    if (!instanceId || !productkey || !deviceName || !deviceSecret || !host) {
+        node.error('缺少必要的配置参数，无法发送请求');
+        return;
+    }
+
+    // 定义请求参数并将其转换为JSON字符串，去掉空格
+    var bodyString = JSON.stringify({ resourceType }, null, 0);
+    
+    // 计算分钟级时间戳
+    var timestamp = Math.floor(Date.now() / (1000 * 60));
+    
+    // 计算请求签名
+    var signature = createSignature(deviceSecret, path, bodyString, timestamp);
+    
+    if (!signature) {
+        node.error('无法生成请求签名');
+        return;
+    }
+    
+    // 构建请求头
+    var headers = {
+        'Content-Type': 'application/json',
+        'expiryTime': timestamp,
+        'signature': signature
+    };
+    
+    // 发送请求
+    sendRequest(node, host + path, headers, { resourceType }, msg);
+});
     }
 
     // 注册节点类型
@@ -45,11 +55,16 @@ module.exports = function(RED) {
 
 // 创建请求签名
 function createSignature(deviceSecret, path, bodyString, timestamp) {
-    var authStringPrefix = `${path}\n${timestamp}\n${bodyString}`;
-    deviceSecret = Buffer.from(deviceSecret);
-    
-    // 使用设备密钥执行HmacSHA256加密，并将结果以base64格式输出
-    return encodeURIComponent(crypto.createHmac('SHA256', deviceSecret).update(authStringPrefix).digest('base64'));
+    try {
+        var authStringPrefix = `${path}\n${timestamp}\n${bodyString}`;
+        deviceSecret = Buffer.from(deviceSecret);
+        
+        // 使用设备密钥执行HmacSHA256加密，并将结果以base64格式输出
+        return encodeURIComponent(crypto.createHmac('SHA256', deviceSecret).update(authStringPrefix).digest('base64'));
+    } catch (error) {
+        node.error('创建签名失败: ' + error.message, msg);
+        return '';
+    }
 }
 
 // 发送请求并处理响应或错误
